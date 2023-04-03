c3c870ca406547f44ebe53872f283058185061ac
Egor Krivonos <karl11karle@gmail.com>
2023-04-03 11:33:00 +0300

    Конвертер валюты [#41583]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -6,8 +6,15 @@ public class Calculator {
         System.out.println(result);
     }
 
+    public static void minus(int first, int second) {
+        int result = first - second;
+        System.out.println(result);
+    }
+
     public static void main(String[] args) {
-        Calculator.plus(1, 2);
-        Calculator.plus(10, 11);
+        plus(1, 2);
+        plus(10, 11);
+        minus(10, 2);
+
     }
 }

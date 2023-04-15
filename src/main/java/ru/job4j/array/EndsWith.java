b96966150558a72b73519c9c7316a1deb281199d
Egor Krivonos <karl11karle@gmail.com>
2023-04-15 15:21:20 +0300

    no comments[2]

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -3,7 +3,6 @@ package ru.job4j.array;
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
-        /* проверить. что массив word имеет последние элементы одинаковые с post */
         for (int i = 0; i < post.length; i++) {
             if (word[word.length - post.length + i] != post[i]) {
                 result = false;
